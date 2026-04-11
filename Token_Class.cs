e99da0d7db73a8b81969a1136a63b0593f3ee0bc
omarsherif4767 <omarsherif2006@gmail.com>
2026-04-11 23:51:19 +0200

    Update Token_Class.cs

--- Token_Class.cs
+++ Token_Class.cs
@@ -1,4 +1,10 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,19 +14,20 @@ namespace Tiny
 {
     public enum Token_Class
     {
-        // Reserved Words
+        //  Reserved Words
         If, Repeat, Read, Write, Then, Else, ElseIf, End, Return,
-        Int, Float, String,
+        Int, Float, String, Endl, Until,
 
-        // Symbols
+        //  Symbols
         LParanthesis, RParanthesis, Semicolon, Comma,
+        LBrace, RBrace,
 
-        // Operators
+        //  Operators
         Assign, PlusOp, MinusOp, MultiplyOp, DivideOp,
         LessThanOp, GreaterThanOp, EqualOp, NotEqualOp,
         AndOp, OrOp,
 
-        
+        //  General
         Identifier, Number, StringValue
     }
 }
